ef5a93184f80719d8fdf9c945eced4b4bc9400a1
SiphoYawe <yawephillip@gmail.com>
2022-03-10 13:05:55 -0800

    Fixes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - Prints sizes of characters
- * 
- * Return: Always 0 (success)
- */
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *    */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -11,5 +11,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
-}}
+}
